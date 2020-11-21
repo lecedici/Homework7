5729ee4b812f4454a55852ab5c909e7b358ed9eb
xxdima <dimal@mof.gov.il>
2020-11-21 10:22:47 +0200

    .

---
+++ search4errors.py
@@ -0,0 +1,14 @@
+import os
+log_file = "C:\\Temp\\horizon.txt"
+result_file = "C:\\Temp\\error.txt"
+with open(log_file) as fileorg:
+    lines = fileorg.readlines()
+    with open(result_file, "w") as file:
+        file.write(f"\n")
+        for line in lines:
+            err = line.find('ERROR')
+            if err != -1:
+                print(line)
+                file.writelines(line)
+if os.stat(result_file).st_size:
+    exit(1)
